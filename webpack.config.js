c99ac5e41c65d53363d8600837b127b534f245c7
Kate Wood <katewood611@gmail.com>
2016-08-29 23:52:05 -0700

    adds css file and styles weather page

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
       Weather: 'app/components/Weather.jsx',
       WeatherForm: 'app/components/WeatherForm.jsx',
       WeatherMessage: 'app/components/WeatherMessage.jsx',
-      openWeatherMap: 'app/api/openWeatherMap.jsx'
+      openWeatherMap: 'app/api/openWeatherMap.jsx',
+      applicationStyles: 'app/styles/app.css'
     },
     extensions: ['', '.js', '.jsx']
   },
