2c59685999414ba1fb81442abf1f79f0ac7acc7c
Kate Wood <katewood611@gmail.com>
2016-08-29 21:44:19 -0700

    updates example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,6 +8,13 @@ var React = require('react')
 //   }
 // })
 
-var Examples = (props) => <h3>Examples Component</h3>
+var Examples = (props) => {
+  return (
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to the Examples page!</p>
+    </div>
+  )
+}
 
 module.exports = Examples
