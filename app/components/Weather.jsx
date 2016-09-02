10dd32305a9794d4c0288afc070bbb2e40fdcf4e
Kate Wood <katewood611@gmail.com>
2016-09-01 19:50:19 -0700

    updates weather component

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -12,7 +12,7 @@ var Weather = React.createClass({
   },
   handleSearch: function(location){
     var that = this
-
+    
     this.setState({
       isLoading: true,
       errorMessage: undefined
